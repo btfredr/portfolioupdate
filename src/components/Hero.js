28c525d390f432ee243d43c840307da3d2699081
Fredrik <fredrikf2000@gmail.com>
2021-08-08 16:19:38 +0200

    finished hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,11 +1,30 @@
+import styled from "styled-components";
+import Video2 from "../assets/videos/video2.mp4";
+
 const Hero = () => {
   return (
     <div className="hero">
       <div className="hero__container">
-        <div className="hero__bg"></div>
+        <div className="hero__bg">
+          <VideoBg autoPlay loop muted src={Video2} type="video/mp4" />
+        </div>
+
+        <div className="hero__content">
+          <h1 className="hero__h1">Fredrik Fordelsen</h1>
+          <p className="hero__p">Frontend Developer</p>
+        </div>
       </div>
     </div>
   );
 };
 
 export default Hero;
+
+export const VideoBg = styled.video`
+  width: 100%;
+  height: 100%;
+  -o-object-fit: cover;
+  object-fit: cover;
+  background-color: #232a34;
+  opacity: 1;
+`;
